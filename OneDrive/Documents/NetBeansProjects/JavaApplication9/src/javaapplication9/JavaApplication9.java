7a908ad87ad90c8e92c9ac19edff8bbcb5ac8c35
Tanya <ts519@snu.edu.in>
2024-07-19 16:22:28 +0530

    first commit

---
+++ OneDrive/Documents/NetBeansProjects/JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication9;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tanya
+ */
+public class JavaApplication9  {//method2
+       public JavaApplication9()
+       {
+           JFrame jFrame= new JFrame();
+            jFrame.setLayout(null);
+            jFrame.setSize(400,400);
+            jFrame.setVisible(true);
+        }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new JavaApplication9();
+        // TODO code application logic here
+    }
+    
+}
